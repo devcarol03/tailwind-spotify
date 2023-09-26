13e14acf54e7d173f96c2aee0dd014360aff5fa5
devcarol03 <devcarol03@gmail.com>
2023-09-26 22:39:59 +0100

    update1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,82 @@
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight} from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <div>
-        <aside>sidebar</aside>
-        <main>main</main>
+    <div className="h-screen flex flex-col">
+      <div className='flex flex-1'>
+        <aside className='w-72 bg-zinc-950 p-6'>
+          <div className='flex items-center gap-2'>
+            <div className='w-3 h-3 bg-red-500 rounded-full' />
+            <div className='w-3 h-3 bg-yellow-500 rounded-full' />
+            <div className='w-3 h-3 bg-green-500 rounded-full' />
+          </div>
+          
+          <nav className='space-y-5 mt-10'>
+            <a href="" className='flex items-center gap-2 text-sm font-semibold text-zinc-200'>
+              <HomeIcon />
+              Home
+            </a>
+            <a href="" className='flex items-center gap-2 text-sm font-semibold text-zinc-200'>
+              <Search />
+              Search
+            </a>
+            <a href="" className='flex items-center gap-2 text-sm font-semibold text-zinc-200'>
+              <Library />
+              Your Library
+            </a>
+          </nav>
+
+          <nav className='mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3'>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'> latim avançado</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'> pique brasileirin</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'> Liked Songs</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'> Rick play</a>
+          </nav>
+        </aside>
+        <main className='flex-1 p-6'>
+          <div className='flex items-center gap-4'>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronLeft />
+            </button>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className='font-semibold text-3xl mt-10'> Good Afternoon</h1>
+
+          <div className='grid grid-cols-3 gap-4 mt-4'>
+            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
+              <strong> Playlist</strong>
+            </a>
+            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
+              <strong> Playlist</strong>
+            </a>
+            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
+              <strong> Playlist</strong>
+            </a>
+            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
+              <strong> Playlist</strong>
+            </a>
+            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
+              <strong> Playlist</strong>
+            </a>
+            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
+              <strong> Playlist</strong>
+            </a>
+          </div>
+        </main>
       </div>
-      <footer>footer</footer>
+      <footer className='bg-zinc-800 border-t border-zinc-700 p-6'>
+        footer
+      </footer>
     </div>
   )
 }
