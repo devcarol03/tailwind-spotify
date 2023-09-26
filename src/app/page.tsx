b5998262cf7011da2fab18a3fa5357f0ac39aa8d
devcarol03 <devcarol03@gmail.com>
2023-09-26 19:24:16 +0100

    commit1

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <div>
+        <aside></aside>
+        <main></main>
+      </div>
+      <footer></footer>
+    </div>
+  )
+}
