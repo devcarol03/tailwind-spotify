dcc4ee38dafab130f5f31635e18d3d50f621a543
devcarol03 <devcarol03@gmail.com>
2023-09-28 11:47:44 +0100

    update2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight} from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2Icon, ListMusic, MonitorSpeaker, Volume2} from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -44,38 +44,122 @@ export default function Home() {
             </button>
           </div>
 
-          <h1 className='font-semibold text-3xl mt-10'> Good Afternoon</h1>
+          <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
 
           <div className='grid grid-cols-3 gap-4 mt-4'>
-            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href="#"className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
               <strong> Playlist</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full  bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href="#"className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
               <strong> Playlist</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full  bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href="#"className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
               <strong> Playlist</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full  bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href="#"className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
               <strong> Playlist</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full  bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href="#"className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
               <strong> Playlist</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full  bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a className='bg-white/10 rounded flex item-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href="#"className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/album.jpg" width={104} height={104} alt="Capa do álbum MDID" />
               <strong> Playlist</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full  bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
           </div>
+
+          <h2 className='font-semibold text-2xl mt-10'>Made for carol</h2>
+
+          <div className='grid grid-cols-8 gap-4 mt-4'>
+            <a className='bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa do álbum MDID" />
+              <strong className='font-semibold'>Playlist</strong>
+              <span className='text-sm text-zinc-400'>ProfJam</span>
+            </a>
+            <a className='bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa do álbum MDID" />
+              <strong className='font-semibold'>Playlist</strong>
+              <span className='text-sm text-zinc-400'>ProfJam</span>
+            </a>
+            <a className='bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa do álbum MDID" />
+              <strong className='font-semibold'>Playlist</strong>
+              <span className='text-sm text-zinc-400'>ProfJam</span>
+            </a>
+            <a className='bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa do álbum MDID" />
+              <strong className='font-semibold'>Playlist</strong>
+              <span className='text-sm text-zinc-400'>ProfJam</span>
+            </a>
+            <a className='bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa do álbum MDID" />
+              <strong className='font-semibold'>Playlist</strong>
+              <span className='text-sm text-zinc-400'>ProfJam</span>
+            </a>
+          </div>
+
         </main>
       </div>
-      <footer className='bg-zinc-800 border-t border-zinc-700 p-6'>
-        footer
+      <footer className='bg-zinc-800 border-t border-zinc-700 p-5 flex items-center justify-between'>
+        <div className='flex items-center gap-3'>
+          <Image src="/album.jpg" width={65} height={65} alt="Capa do álbum MDID" />
+          <div className='flex flex-col'>
+            <strong className='text-normal'>INTRO</strong>
+            <span className='text-xs text-zinc-400'>ProfJam</span>
+          </div>
+        </div>
+
+        <div className='flex flex-col items-center gap-2'>
+          <div className='flex items-center gap-6'>
+            <Shuffle size={20} className='text-zinc-200' />
+            <SkipBack size={20} className='text-zinc-200' />
+            <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full  bg-white text-black'>
+                <Play />
+            </button>
+            <SkipForward size={20} className='text-zinc-200' />
+            <Repeat size={20} className='text-zinc-200' />
+
+          </div>
+
+          <div className='flex items-center gap-3'>
+            <span className='text-xs text-zinc-400'>0:22</span>
+            <div className='h1 rounded-full w-96 bg-zinc-600'>
+              <div className='bg-zinc-200 w-6 h-1 rounded-full'></div>
+            </div>
+            <span className='text-xs text-zinc-400'>1:23</span>
+          </div>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Mic2Icon size={20} />
+          <ListMusic size={20} />
+          <MonitorSpeaker size={20} />
+          <div>
+            <Volume2 size={20} />
+          </div>
+        </div>
       </footer>
     </div>
   )
